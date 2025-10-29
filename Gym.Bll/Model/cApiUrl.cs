f2d755aa5ef40e6fa5a36e725fb5007bc41915d6
Julian <137653034+HealSturm@users.noreply.github.com>
2025-10-29 15:52:38 -0600

    Agregar archivos de proyecto.

---
+++ Gym.Bll/Model/cApiUrl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym.Bll.Model
+{
+    public class cApiUrl
+    {
+        private string WebApiUrl = "https://localhost:7003/";
+        public string getWebApiUrl()
+        { return WebApiUrl; 
+        
+        }
+
+    }
+}
---
+++ Gym.app/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Gym.app
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
