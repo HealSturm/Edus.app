a03cae8705b8179c9d5eb0761b6a74676c128ef2
Félix Daniel Alvarado <feldan2002@gmail.com>
2025-11-04 14:45:38 -0600

    Controladores

--- Gym.WebAPI/Controllers/ClienteMedicamentoController.cs
+++ Gym.WebAPI/Controllers/ClienteMedicamentoController.cs
@@ -9,7 +9,139 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Edus.WebAPI.Controllers
 {
-    public class ClienteMedicamentoController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteMedicamentoController : ControllerBase
     {
+        [HttpGet]
+        [Route("getClienteMedicamento")]
+        public async Task<ActionResult<List<cClienteMedicamento>>> getClienteMedicamento()
+        {
+            try
+            {
+                dbConection db = new dbConection();
+                dsClienteMedicamento mdsCat = new dsClienteMedicamento(db.sqlConection);
+                List<cClienteMedicamento> mLista = await mdsCat.getClienteMedicamento();
+                return Ok(mLista);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //****************************************************************************************************
+        [HttpPost]
+        [Route("insertarClienteMedicamento")]
+        public async Task<ActionResult<bool>> insertarClienteMedicamento([FromBody] cClienteMedicamento _ClienteMedicamento)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Datos del Medicamiento cliente inválidos.");
+
+            try
+            {
+                dbConection db = new dbConection();
+                dsClienteMedicamento mdsCat = new dsClienteMedicamento(db.sqlConection);
+
+                bool resultado = await mdsCat.insertarClienteMedicamento(_ClienteMedicamento);
+
+                if (resultado)
+                    return Ok(true);
+                else
+                    return BadRequest("No se pudo insertar el Medicamiento cliente.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error al insertar el Medicamiento cliente: " + ex.Message);
+            }
+        }
+        //****************************************************************************************************
+        [HttpPut]
+        [Route("actualizarClienteMedicamento")]
+        public async Task<ActionResult<string>> actualizarClienteMedicamento([FromBody] cClienteMedicamento _ClienteMedicamento)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                dbConection db = new dbConection();
+                dsClienteMedicamento ndsCliente = new dsClienteMedicamento(db.sqlConection);
+
+                if (await ndsCliente.actualizarClienteMedicamento(_ClienteMedicamento) == true)
+                {
+                    return Ok("Medicamiento Cliente actualizado correctamente.");
+                }
+                else
+                {
+                    return BadRequest("No se pudo actualizar el Medicamiento cliente.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    error = ex.Message,
+                    inner = ex.InnerException?.Message
+                });
+            }
+        }
+        //******************************************************************************************************
+        [HttpDelete]
+        [Route("borrarClienteMedicamento/{Identificacion}")]
+        public async Task<ActionResult<string>> borrarClienteMedicamento(int Identificacion)
+        {
+            try
+            {
+                dbConection db = new dbConection();
+                dsClienteMedicamento ndsClienteMedicamento = new dsClienteMedicamento(db.sqlConection);
+
+                cClienteMedicamento ClienteMedicamento = new cClienteMedicamento();
+                ClienteMedicamento.Identificacion = Identificacion;
+
+                if (await ndsClienteMedicamento.borrarClienteMedicamento(ClienteMedicamento))
+                {
+                    return Ok("Medicamiento Cliente eliminado correctamente.");
+                }
+                else
+                {
+                    return NotFound("No se encontró un Medicamiento cliente con esa identificación.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error al eliminar Medicamiento cliente: {ex.Message}");
+            }
+        }
+
+
+
+
+
     }
-}
+}           
+
+
+
+
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Gym.WebAPI/Controllers/MedicamentoController.cs
+++ Gym.WebAPI/Controllers/MedicamentoController.cs
@@ -8,7 +8,115 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Edus.WebAPI.Controllers
 {
-    public class MedicamentoController
-    {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicamentoController : ControllerBase
+    {       
+            [HttpGet]
+            [Route("getMedicamento")]
+            public async Task<ActionResult<List<cMedicamento>>> getMedicamento()
+            {
+                try
+                {
+                    dbConection db = new dbConection();
+                dsMedicamento mdsCat = new dsMedicamento(db.sqlConection);
+                    List<cMedicamento> mLista = await mdsCat.getMedicamento();
+                    return Ok(mLista);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+
+            //****************************************************************************************************
+            [HttpPost]
+            [Route("insertarMedicamento")]
+            public async Task<ActionResult<bool>> insertarMedicamento([FromBody] cMedicamento _Medicamento)
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest("Datos del Medicamento inválidos.");
+
+                try
+                {
+                    dbConection db = new dbConection();
+                dsMedicamento mdsCat = new dsMedicamento(db.sqlConection);
+
+                    bool resultado = await mdsCat.insertarMedicamento(_Medicamento);
+
+                    if (resultado)
+                        return Ok(true);
+                    else
+                        return BadRequest("No se pudo insertar el Medicamento.");
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Error al insertar el Medicamento: " + ex.Message);
+                }
+            }
+            //****************************************************************************************************
+            [HttpPut]
+            [Route("actualizarMedicamento")]
+            public async Task<ActionResult<string>> actualizarMedicamento([FromBody] cMedicamento _Medicamento)
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                try
+                {
+                    dbConection db = new dbConection();
+                dsMedicamento ndsMedicamento = new dsMedicamento(db.sqlConection);
+
+                    if (await ndsMedicamento.actualizarMedicamento(_Medicamento) == true)
+                    {
+                        return Ok("Medicamento actualizado correctamente.");
+                    }
+                    else
+                    {
+                        return BadRequest("No se pudo actualizar el Medicamento.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new
+                    {
+                        error = ex.Message,
+                        inner = ex.InnerException?.Message
+                    });
+                }
+            }
+            //******************************************************************************************************
+            [HttpDelete]
+            [Route("borrarclienteMedicamento/{Identificacion}")]
+            public async Task<ActionResult<string>> borrarMedicamento(int Identificacion)
+            {
+                try
+                {
+                    dbConection db = new dbConection();
+                dsMedicamento ndsMedicamento = new dsMedicamento(db.sqlConection);
+
+                cMedicamento Medicamento = new cMedicamento();
+                Medicamento.Identificacion = Identificacion;
+
+                    if (await ndsMedicamento.borrarMedicamento(Medicamento))
+                    {
+                        return Ok("Medicamento eliminado correctamente.");
+                    }
+                    else
+                    {
+                        return NotFound("No se encontró un Medicamento con esa identificación.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest($"Error al eliminar Medicamento: {ex.Message}");
+                }
+            }
+
+
+
+
+
+        }
     }
-}
+
